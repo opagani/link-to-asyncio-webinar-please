4faead949db1b756f8c124a6a6e332273c2cb0d0
Reuven Lerner <reuven@lerner.co.il>
2024-09-09 20:50:17 +0300

    gitautosync update 72

--- ex1.py
+++ ex1.py
@@ -4,14 +4,17 @@ import asyncio
 
 async def up(maximum):
     for i in range(maximum):
+        await asyncio.sleep(0.1)
         print(f'up, {i}')
 
 async def down(maximum):
     for i in range(maximum, 0, -1):
+        await asyncio.sleep(0.1)
         print(f'down, {i}')
 
 async def powers(n):
     for i in range(2, 8):
+        await asyncio.sleep(0.1)
         print(f'powers, {n} ** {i} = {n**i}')
 
 async def main():
