634a6451098f1c3a0505b307db62e4fb4eb1b268
Reuven Lerner <reuven@lerner.co.il>
2024-09-09 20:15:01 +0300

    gitautosync update 37

--- asy03.py
+++ asy03.py
@@ -4,7 +4,10 @@ import asyncio
 import time
 
 async def hello():
-    time.sleep(1)
+
+    # if something is going to take a long time, then I should say "await"
+    # that will allow another coroutine to run instead of us
+    await time.sleep(1)
     print('Hello!')
 
 # to put something on the event loop, we can say asyncio.run()
