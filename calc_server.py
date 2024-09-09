0c81b470a16dc69f06d2b9f3ad87868bb0dde8ef
Reuven Lerner <reuven@lerner.co.il>
2024-09-09 22:09:13 +0300

    gitautosync update 150

--- calc_server.py
+++ calc_server.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+import asyncio
+
+def calc(s):   # gets a string with a math expression, and returns the result
+    first, op, second = s.split()
+
+    first = int(first)
+    second = int(second)
+
+    if op == '+':
+        result = first + second
+    elif op == '-':
+        result = first - second
+    else:
+        result = f'Operator {op} not supported'
+
+    return result
+
+async def main():
+    server = await asyncio.start_server(handle_client,
+                                        'localhost',
+                                        6789)
+
+    async with server:
+        await server.serve_forever()
