cb5d1d6127cd9fdfecaee312b6de6b9ee14a701b
Reuven Lerner <reuven@lerner.co.il>
2024-09-09 21:46:58 +0300

    gitautosync update 132

--- geturls.py
+++ geturls.py
@@ -3,6 +3,7 @@
 # I want to iterate over a list of URLs, and retrieve each one,
 # counting the number of bytes on that page, and then print it out.
 
+import asyncio
 import aiohttp
 
 all_urls = ['https://python.org',
