be39982a28469df11d7b951244884b5bb1fccbdc
Reuven Lerner <reuven@lerner.co.il>
2024-09-09 20:55:43 +0300

    gitautosync update 77

--- ex1.5.py
+++ ex1.5.py
@@ -2,20 +2,22 @@
 
 import asyncio
 
+output = []
+
 async def up(maximum):
     for i in range(maximum):
         await asyncio.sleep(0.1)
-        print(f'up, {i}')
+        output.append(f'up, {i}')
 
 async def down(maximum):
     for i in range(maximum, 0, -1):
         await asyncio.sleep(0.1)
-        print(f'down, {i}')
+        output.append(f'down, {i}')
 
 async def powers(n):
     for i in range(2, 8):
         await asyncio.sleep(0.1)
-        print(f'powers, {n} ** {i} = {n**i}')
+        output.append(f'powers, {n} ** {i} = {n**i}')
 
 async def main():
     t1 = asyncio.create_task(up(10))
@@ -28,3 +30,4 @@ async def main():
         await one_task
 
 asyncio.run(main())
+print(output)
