9a1da7f035f5898ca283c7ab6ae1a3e105ef556e
Reuven Lerner <reuven@lerner.co.il>
2024-09-09 20:47:42 +0300

    gitautosync update 65

--- ex1.py
+++ ex1.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+import asyncio
+
+async def up(maximum):
+    for i in range(maximum):
+        print(f'up, {i}')
+
+async def down(maximum):
+    for i in range(maximum, 0, -1):
+        print(f'down, {i}')
